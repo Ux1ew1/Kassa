a9cf4e8c9e9bd88659866b31374e26ca219ca1dd
Ux1ew1 <Uxlewl0220@gmail.com>
2026-01-12 20:09:35 +0300

    Linter
    Правка Линтером

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,21 +1,20 @@
-import './MenuItem.css'
+import "./MenuItem.css";
 
 function MenuItem({ item, onAdd }) {
   const handleClick = () => {
-    onAdd(item)
+    onAdd(item);
     // Вибрация на поддерживаемых устройствах
     if (navigator.vibrate) {
-      navigator.vibrate(15)
+      navigator.vibrate(15);
     }
-  }
+  };
 
   return (
     <div className="item" onClick={handleClick}>
       <span className="item-name">{item.name}</span>
       <span className="item-price">{item.price} руб.</span>
     </div>
-  )
+  );
 }
 
-export default MenuItem
-
+export default MenuItem;
